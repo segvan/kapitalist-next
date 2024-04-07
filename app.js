950a656dbce8505d59572a7b0bd59e2448c408f5
Sergii <s.trotsenko@gmail.com>
2024-04-07 20:13:32 +0200

    workers production start

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ cli.nextStart({
   _: [],
 });
 
-nodeCron.schedule("1/30 * * * *", tradeHistoryBot.run); // every 30 minutes (1st minute)
-nodeCron.schedule("2/5 * * * *", pricesBot.run); // every 5 minutes (2nd minute)
-nodeCron.schedule("3/5 * * * *", priceChangeBot.run); // every 5 minutes (3rd minute)
+nodeCron.schedule("5 0,30 * * * *", tradeHistoryBot.run); // every 30 minutes (1st minute)
+nodeCron.schedule("2-57/5 * * * *", pricesBot.run); // every 5 minutes (2nd minute)
+nodeCron.schedule("3-58/5 * * * *", priceChangeBot.run); // every 5 minutes (3rd minute)
+
+setTimeout(function () {
+  process.send('ready');
+}, 1000);
