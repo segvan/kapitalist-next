f04bd90480365586204208440dcea578989718a6
Sergii <s.trotsenko@gmail.com>
2024-04-13 12:28:16 +0200

    front fixes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "@/src/components/LoginForm";
 
 function Login() {
   return (
-    <div className="hero is-fullheight has-background-info-light">
+    <div className="hero is-fullheight">
       <div className="hero-body">
         <div className="container has-text-centered">
           <div className="column is-6 is-offset-3 is-desktop">
--- src/components/tradeHistory/table/TradeHistoryTable.tsx
+++ src/components/tradeHistory/table/TradeHistoryTable.tsx
@@ -81,85 +81,86 @@ export default function TradeHistoryTable({data}: TradeHistoryTableProps) {
       </table>
 
       <h4 className="subtitle">Details</h4>
-
-      <table
-        className="table is-narrow is-hoverable is-striped"
-        onClick={tableClick}
-      >
-        <thead>
-        <tr>
-          <th scope="col">Currency</th>
-          <th className={columnToggle} scope="col">
-            %
-          </th>
-          <th className={columnToggle} scope="col">
-            Invested
-          </th>
-          <th className={columnToggle} scope="col">
-            Current Price
-          </th>
-          <th className={columnToggle} scope="col">
-            Average Price
-          </th>
-          <th className={columnToggle} scope="col">
-            Amount
-          </th>
-          <th scope="col">Current Value</th>
-          <th scope="col">Earnings</th>
-          <th scope="col">Difference</th>
-        </tr>
-        </thead>
-        <tbody>
-        {data.map((trade) => (
-          <tr key={trade.Symbol}>
-            <td>{trade.Symbol}</td>
-            <td className={columnToggle}>
-              {((trade.QuoteQty / invested) * 100).toFixed(2)}
-            </td>
-            <td className={columnToggle}>
-              {trade.QuoteQty.toFixed(2)}
-            </td>
-            <td className={columnToggle}>
-              {trade.CurrentPrice.toFixed(2)}
-            </td>
-            <td className={columnToggle}>
-              {trade.AvgPrice.toFixed(2)}
-            </td>
-            <td className={columnToggle}>{trade.Qty}</td>
-            <td
-              className={
-                "has-text-white " +
-                (trade.CurrentTotalAmount >= trade.QuoteQty
-                  ? "has-background-success"
-                  : "has-background-danger")
-              }
-            >
-              {trade.CurrentTotalAmount.toFixed(2)}
-            </td>
-            <td
-              className={
-                "has-text-white " +
-                (trade.TotalEarnings >= 0
-                  ? "has-background-success"
-                  : "has-background-danger")
-              }
-            >
-              {trade.TotalEarnings.toFixed(2)}
-            </td>
-            <td
-              className={
-                "has-text-white " +
-                (trade.TotalDifference >= 0
-                  ? "has-background-success"
-                  : "has-background-danger")
-              }
-            >
-              {trade.TotalDifference.toFixed(2)} %
-            </td>
+      <div className="is-overflow-y-hidden">
+        <table
+          className="table is-narrow is-hoverable is-striped"
+          onClick={tableClick}
+        >
+          <thead>
+          <tr>
+            <th scope="col">Currency</th>
+            <th className={columnToggle} scope="col">
+              %
+            </th>
+            <th className={columnToggle} scope="col">
+              Invested
+            </th>
+            <th className={columnToggle} scope="col">
+              Current Price
+            </th>
+            <th className={columnToggle} scope="col">
+              Average Price
+            </th>
+            <th className={columnToggle} scope="col">
+              Amount
+            </th>
+            <th scope="col">Current Value</th>
+            <th scope="col">Earnings</th>
+            <th scope="col">Difference</th>
           </tr>
-        ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+          {data.map((trade) => (
+            <tr key={trade.Symbol}>
+              <td>{trade.Symbol}</td>
+              <td className={columnToggle}>
+                {((trade.QuoteQty / invested) * 100).toFixed(2)}
+              </td>
+              <td className={columnToggle}>
+                {trade.QuoteQty.toFixed(2)}
+              </td>
+              <td className={columnToggle}>
+                {trade.CurrentPrice.toFixed(2)}
+              </td>
+              <td className={columnToggle}>
+                {trade.AvgPrice.toFixed(2)}
+              </td>
+              <td className={columnToggle}>{trade.Qty}</td>
+              <td
+                className={
+                  "has-text-white " +
+                  (trade.CurrentTotalAmount >= trade.QuoteQty
+                    ? "has-background-success"
+                    : "has-background-danger")
+                }
+              >
+                {trade.CurrentTotalAmount.toFixed(2)}
+              </td>
+              <td
+                className={
+                  "has-text-white " +
+                  (trade.TotalEarnings >= 0
+                    ? "has-background-success"
+                    : "has-background-danger")
+                }
+              >
+                {trade.TotalEarnings.toFixed(2)}
+              </td>
+              <td
+                className={
+                  "has-text-white " +
+                  (trade.TotalDifference >= 0
+                    ? "has-background-success"
+                    : "has-background-danger")
+                }
+              >
+                {trade.TotalDifference.toFixed(2)} %
+              </td>
+            </tr>
+          ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
